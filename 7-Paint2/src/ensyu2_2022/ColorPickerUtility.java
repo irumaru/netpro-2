4afd9644ba7a615d8f21be0763edbc26c531decc
irumaru <46730701+irumaru@users.noreply.github.com>
2022-11-23 00:48:11 +0900

    完成

--- 7-Paint2/src/ensyu2_2022/ColorPickerUtility.java
+++ 7-Paint2/src/ensyu2_2022/ColorPickerUtility.java
@@ -4,35 +4,24 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 
-//import java.awt.FlowLayout;
 import javax.swing.JColorChooser;
-
-//import ensyu2_2022.ButtonUtility;
 
 public class ColorPickerUtility extends ButtonUtility{
 	Frame frame;
 	Color color;
 	
 	ColorPickerUtility(Frame frame){
-		super(frame, "色の選択", "詳細");
+		super(frame, "色指定", "詳細");
 		this.frame = frame;
+		this.color = new Color(100, 255, 255);
 	}
 	
 	@Override public void actionPerformed(ActionEvent e) {
-		/*
-		Frame f = new Frame();
-		f.setSize(800,600);
-		//f.setLayout(null);
-		//f.setLayout(new FlowLayout(FlowLayout.CENTER));
-		f.setTitle("カラーピッカー");
-		f.addWindowListener(new WindowAdapter(){
-			@Override public void windowClosing(WindowEvent e){
-				System.exit(0);
-			}});
-		f.setVisible(true);
-		*/
-		//JColorChooser colorchooser = new JColorChooser();
-		this.color = JColorChooser.showDialog(this.frame, "色の選択", Color.white);
+		Color color = JColorChooser.showDialog(this.frame, "色の選択", Color.white);
+		
+		//取り消しなどで色が取得できないとき
+		if(color != null)
+			this.color = color;
 	}
 	
 	public Color getColor() {
--- 7-Paint2/src/ensyu2_2022/Paint.java
+++ 7-Paint2/src/ensyu2_2022/Paint.java
@@ -24,6 +24,7 @@ public class Paint extends Frame implements MouseListener,MouseMotionListener {
 	TextFieldUtility height;
 	ChoiceFieldUtility fill;
 	ChoiceFieldUtility shape;
+	//ChoiceFieldUtility sampleColor;
 	ColorPickerUtility color;
 	
 	int objPrintCount = 0;
@@ -54,130 +55,59 @@ public class Paint extends Frame implements MouseListener,MouseMotionListener {
 		fill = new ChoiceFieldUtility(this, "塗りつぶし", new String []{"なし", "塗りつぶし"});
 		shape = new ChoiceFieldUtility(this, "図形", new String []{"円形", "四角形"});
 		color = new ColorPickerUtility(this);
-		
-		//JColorChooser colorchooser = new JColorChooser();
-		//Color color = colorchooser.showDialog(this, "色の選択", Color.white);
-		
-		//System.out.println(color);
 	}
 	
 	//描画(フレームごと)
 	@Override public void paint(Graphics g){
-		//設定(仮)
-		int objLimitCount = size.getValue();//最大表示オブジェクト数 0で制限なし
-		boolean objCountOut = true;
-		
-		//初期化
-		
-		
-		
-		//円の表示数をカウント
-		if(objLimitCount == 0)
-			objPrintCount = objList.size();
-		else if(objLimitCount < objList.size())
-			objPrintCount = objLimitCount;
-		else
-			objPrintCount = objList.size();
-		
 		//円を表示(レンダリング)
 		Figure f;
-		for(int i = 0; i < objPrintCount; i ++) {
+		System.out.println(objList.size());
+		for(int i = objList.size() - 1; i >= 0; i --) {
 			f = objList.get(i);
 			f.paint(g);
 		}
 		
-		//何してるのか不明
+		//forの繰り返し条件が正しい限り不要
 		//if(obj != null) obj.paint(g);
-		
-		//コンソール出力
-		if(objCountOut)
-			System.out.printf("制限: %d個\n図形: %d\n", objPrintCount, shape.getChoice());
-		
-		//プリントディバッグ
-		//System.out.printf("最後の円のサイズ: %d\n", tmp);
 	}
 	
 	//イベント->マウスプレス
 	@Override public void mousePressed(MouseEvent e){
-		/*
-		x = e.getX();
-		y = e.getY();
-		obj = new Circle();
-		obj.moveto(x, y);
-		repaint();
-		*/
-		
-		//円
+		//図形
 		int choice = shape.getChoice();
 		if(choice == 0) {
 			obj = new Circle();
 		}else if(choice == 1) {
 			obj = new Square();
 		}
-		
 		//座標
 		x = e.getX();
 		y = e.getY();
 		obj.moveto(x, y);
-		
 		//色をつける
-		//100,255,255 リムル
 		obj.setColor(color.getColor());
-		
 		//塗りつぶし
 		obj.setFill(fill.getChoice() == 1);
-		
 		//サイズの指定
 		obj.setSize(width.getValue(), height.getValue());
-		
 		//リストに追加
 		objList.add(0, obj);
+		
+		//円の数が制限されているときは、古い円から削除
+		if(size.getValue() != 0) {
+			while(objList.size() > size.getValue()) {
+				objList.remove((int)size.getValue());
+			}
+		}
 		
 		//表示(レンダリング)
 		repaint();
 	}
 	//イベント->マウスリリース
-	@Override public void mouseReleased(MouseEvent e){
-		/*
-		x = e.getX();
-		y = e.getY();
-		obj.moveto(x, y);
-		objList.add(obj);
-		obj = null;
-		repaint();
-		*/
-		
-		/*
-		//離したとき
-		x = e.getX();
-		y = e.getY();
-		obj = new Circle();
-		obj.moveto(x, y);
-		objList.add(0, obj);
-		repaint();
-		*/
-	}
+	@Override public void mouseReleased(MouseEvent e){}
 	@Override public void mouseClicked(MouseEvent e){}
 	@Override public void mouseEntered(MouseEvent e){}
 	@Override public void mouseExited(MouseEvent e){}
-	//イベント->マウスドラッグ
-	@Override public void mouseDragged(MouseEvent e) {
-		/*
-		x = e.getX();
-		y = e.getY();
-		if(obj != null) obj.moveto(x, y);
-		repaint();
-		*/
-		
-		/*
-		//連続
-		x = e.getX();
-		y = e.getY();
-		obj = new Circle();
-		obj.moveto(x, y);
-		objList.add(0, obj);
-		repaint();
-		*/
-	}
+	@Override public void mouseDragged(MouseEvent e) {}
 	@Override public void mouseMoved(MouseEvent e){}
 }
