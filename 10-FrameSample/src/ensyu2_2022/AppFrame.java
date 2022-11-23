fce320d9d85970578e7ad8cd0d74c19f1f2d7e3f
irumaru <46730701+irumaru@users.noreply.github.com>
2022-11-23 14:26:39 +0900

    10回の追加

---
+++ 10-FrameSample/src/ensyu2_2022/AppFrame.java
@@ -0,0 +1,15 @@
+package ensyu2_2022;
+
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+
+public class AppFrame extends Frame{
+	@Override public void paint(Graphics g) {
+		g.setColor(Color.red);
+		g.drawString("Hello Java!", 70, 120);
+		g.drawRect(50, 50, 100, 100);
+		
+		System.out.println("paint");
+	}
+}
