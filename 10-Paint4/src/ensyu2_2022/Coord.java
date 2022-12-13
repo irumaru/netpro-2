5e18ea66e69cdba8db66a82cbce3e9032ecb8250
irumaru <46730701+irumaru@users.noreply.github.com>
2022-12-13 15:53:55 +0900

    n点図形の点関係をcoordへ移動

--- 10-Paint4/src/ensyu2_2022/Coord.java
+++ 10-Paint4/src/ensyu2_2022/Coord.java
@@ -3,12 +3,16 @@
 package ensyu2_2022;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 //Coordクラスの宣言
 //図形の基準点に関することを記述
 public class Coord implements Serializable{
 	//x,yプロパティを宣言
 	int x, y;
+	ArrayList<Integer> coordXList = new ArrayList<Integer>();
+	ArrayList<Integer> coordYList = new ArrayList<Integer>();
+	Integer forcusX, forcusY, count;
 	
 	//x,yプロパティを0で初期化
 	Coord(){
@@ -26,5 +30,50 @@ public class Coord implements Serializable{
 		//xはメソッド内の変数、this.xはプロパティ
 		this.x = x;
 		this.y = y;
+	}
+	
+	public void setXY(Integer x, Integer y) {
+		coordXList.add(x);
+		coordYList.add(y);
+	}
+	
+	public void forcusXY(int x, int y) {
+		forcusX = x;
+		forcusY = y;
+	}
+	
+	private int[] getIntArray(ArrayList<Integer> coords, Integer forcus, Integer point) {
+		int[] coordsInt = new int[coords.size() + 2];
+		
+		coordsInt[0] = point;
+		System.out.println(point);
+		for(int i = 0; i < coords.size(); i ++) {
+			coordsInt[i + 1] = coords.get(i);
+		}
+		
+		count = coords.size() + 1;
+		
+		if(forcus != null) {
+			coordsInt[coords.size() + 1] = forcus;
+			count ++;//forcus分のカウントを追加
+			
+			//仮の点は1度のみ表示
+			//ダブルクリックの2回目の点を無効化
+			forcus = null;
+		}
+		
+		return coordsInt;
+	}
+	
+	protected int[] getCoordXs() {
+		int[] array = getIntArray(coordXList, forcusX, x);
+		forcusX = null;
+		return array;
+	}
+	
+	protected int[] getCoordYs() {
+		int[] array = getIntArray(coordYList, forcusY, y);
+		forcusY = null;
+		return array;
 	}
 }
