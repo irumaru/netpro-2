e19f68556f7a9a08af5c756a01c3858c0e6954bd
irumaru <46730701+irumaru@users.noreply.github.com>
2022-12-16 01:31:40 +0900

    コメントの追加

--- 10-Paint4/src/ensyu2_2022/CustomMath.java
+++ 10-Paint4/src/ensyu2_2022/CustomMath.java
@@ -1,6 +1,9 @@
 package ensyu2_2022;
 
+//計算補助クラス
+//Mathクラスの補助
 public class CustomMath {
+	//配列の中から最小値を求める
 	public static int min(int count, int array[]) {
 		int min = array[0];
 		for(int i = 1; i < count; i ++) {
@@ -12,6 +15,7 @@ public class CustomMath {
 		return min;
 	}
 	
+	//配列の中から最大値を求める
 	public static int max(int count, int array[]) {
 		int max = array[0];
 		for(int i = 1; i < count; i ++) {
